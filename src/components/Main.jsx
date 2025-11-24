0fe6a17912c8fbc8c6973923b3bf5b0cf590e584
me <tkutlesa@gmail.com>
2025-11-24 14:25:14 -0500

    Add deployment script and update dependencies; refactor Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,17 @@
-import { useState } from 'react'
-import { Grid } from './grid/Grid'
-import { Columns } from './inputs/Columns'
-import { Rows } from './inputs/Rows'
-import { Width } from './inputs/Width'
+import { Grid } from "./grid/Grid";
+import { Columns } from "./inputs/Columns";
+import { Rows } from "./inputs/Rows";
+import { Width } from "./inputs/Width";
 
-
-function Main() {
-       
-    return (
-        <>
-            
-            <Width />
-            <Columns
-            
-            />
-            <Rows
-            
-            />
-            <div>
-                <Grid/>
-
-            </div>
-        </>
-    )
+export default function Main() {
+  return (
+    <>
+      <Width />
+      <Columns />
+      <Rows />
+      <div>
+        <Grid />
+      </div>
+    </>
+  );
 }
-
-export default Main
